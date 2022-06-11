531f26c0d69f3027fe235f753f32d11f9274763e
Goodsol Lee <gslee2@netlab.snu.ac.kr>
2022-06-11 22:13:05 +0900

    RL agent added

--- bdq.py
+++ bdq.py
@@ -3,12 +3,15 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 import random
+import easydict
 from torch.optim import lr_scheduler
+from datetime import datetime
 
 from agents.common.utils import *
 from agents.common.buffers import *
 from agents.common.networks import *
 from agents.common.dueling import *
+from agents.dqn import *
 
 class BranchingQNetwork(nn.Module):
 
@@ -19,26 +22,30 @@ class BranchingQNetwork(nn.Module):
         self.ac_dim = ac_dim
         self.n = n 
 
-        self.model = nn.Sequential(nn.Linear(obs, 512), 
+        self.model = nn.Sequential(nn.Linear(obs, 8), 
                                    nn.ReLU(),
-                                   nn.Linear(512, 256), 
+                                   nn.Linear(8, 4), 
                                    nn.ReLU())
 
-        self.value_head = nn.Sequential(nn.Linear(256, 128),
-                                        nn.ReLU(),
-                                        nn.Linear(128, 1))
+        self.value_head = nn.Linear(4, 1)
         
-        self.adv_heads = nn.ModuleList([nn.Sequential(nn.Linear(256, 128),
-                                        nn.ReLU(),
-                                        nn.Linear(128, n)) for i in range(ac_dim)])
+        self.adv_heads = nn.ModuleList([nn.Linear(4, n) for i in range(ac_dim)])
 
     def forward(self, x): 
+        #print('Forward ', x,  x.shape) 
 
         out = self.model(x)
+        #print('Forward1 ',  x.shape) 
         value = self.value_head(out)
+        #print('Forward2 ',  x.shape) 
         advs = torch.stack([l(out) for l in self.adv_heads], dim = 1)
-        
-        q_val = value.unsqueeze(2) + advs - advs.mean(2, keepdim = True )
+        #print('Forward3 ', x,  x.shape, advs.shape, advs) 
+
+        test =  advs.mean(2, keepdim = True)
+        #print('Shape: ', advs.shape, test.shape, value.shape, value.unsqueeze(2).shape)
+
+        q_val = value.unsqueeze(2) + advs - advs.mean(2, keepdim = True)
+        #print('Shape 2: ', advs.shape, test.shape, value.shape, value.unsqueeze(2).shape)
 
         return q_val
 
@@ -46,7 +53,7 @@ class Agent(object):
    """An implementation of the Deep Q-Network (DQN), Double DQN agents."""
 
    def __init__(self,
-                env,
+                #env,
                 args,
                 device,
                 obs_dim,
@@ -55,16 +62,16 @@ class Agent(object):
                 steps=0,
                 gamma=0.99,
                 epsilon=1.0,
-                epsilon_decay=0.995,
-                buffer_size=int(1e4),
+                epsilon_decay=0.98,
+                buffer_size=int(500),
                 batch_size=64,
-                target_update_step=100,
+                target_update_step=50,
                 eval_mode=False,
                 q_losses=list(),
                 logger=dict(),
    ):
 
-      self.env = env
+      #self.env = env
       self.args = args
       self.device = device
       self.obs_dim = obs_dim
@@ -81,8 +88,22 @@ class Agent(object):
       self.q_losses = q_losses
       self.logger = logger
 
+      self.save_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+      ## reward 
+      self.target_fps = 30
+      self.max_res = 38.4
+      self.beta1 = 0.8
+      self.beta2 = 0.2
+      self.current_buffer_index = 0
+      self.max_current_buffer_size = 5
+      self.current_buffer = [None] * self.max_current_buffer_size
+
       # Main network
       self.qf = BranchingQNetwork(self.obs_dim, self.act_dim, self.act_num).to(self.device)
+      if args.load_saved == True:
+         print('Load saved')
+         self.qf.load_state_dict(torch.load(self.args.load_path))
       # Target network
       self.qf_target = BranchingQNetwork(self.obs_dim, self.act_dim, self.act_num).to(self.device)
       
@@ -90,25 +111,36 @@ class Agent(object):
       hard_target_update(self.qf, self.qf_target)
 
       # Create an optimizer
-      self.qf_optimizer = optim.Adam(self.qf.parameters(), lr=1e-3)
+      self.qf_optimizer = optim.Adam(self.qf.parameters(), lr=0.05)
 
       # Experience buffer
       self.replay_buffer = ReplayBuffer(self.obs_dim, self.act_dim, self.buffer_size, self.device)
 
-   def select_action(self, obs):
+   def select_action(self, obs, data):
       """Select an action from the set of available actions."""
       # Decaying epsilon
       self.epsilon *= self.epsilon_decay
-      self.epsilon = max(self.epsilon, 0.01)
+      self.epsilon = max(self.epsilon, 0.02)
+      action = np.array([0,0])
 
-      if np.random.rand() <= self.epsilon:
+      if np.random.rand() <=  self.epsilon:
          # Choose a random action with probability epsilon
-         return np.random.randint(self.act_num)
+         action =  np.random.randint(self.act_num, size = self.act_dim)
+         #print('Random action: ', action)
       else:
-        # Choose the action with highest Q-value at the current state
+         # Choose the action with highest Q-value at the current state
          q_value = self.qf(obs)
          q_argmax = torch.argmax(q_value.squeeze(0), dim = 1)
-         return q_argmax.detach().cpu().numpy()
+         action =  q_argmax.detach().cpu().numpy()
+         #print('RL action: ', action)
+
+      # cool down action
+      if data['modem_temp']/1000 >= 51:
+         action = np.array([0, 0])
+
+      #print('Action: ', action, data['modem_temp']/1000)
+
+      return action
 
    def train_model(self):
       batch = self.replay_buffer.sample(self.batch_size)
@@ -119,23 +151,32 @@ class Agent(object):
       done = batch['done']
 
       if 0: # Check shape of experiences
-         print("obs1", obs1.shape)
-         print("obs2", obs2.shape)
+         print("obs1", obs1.shape, obs1)
+         print("obs2", obs2.shape, self.qf(obs2).shape, torch.argmax(self.qf(obs2), dim = 2))
          print("acts", acts.shape)
+         print("refine acts", acts.reshape(obs1.shape[0],-1,1).shape,  self.qf(obs1).shape)
          print("rews", rews.shape)
          print("done", done.shape)
 
       # Prediction Q(s)
-      q = self.qf(obs1).gather(self.act_dim, acts.long()).squeeze(1)
+      acts = acts.reshape(obs1.shape[0],-1,1)
+      q = self.qf(obs1).gather(2, acts.long()).squeeze(-1)
       
       # Target for Q regression
       if self.args.algo == 'dqn':      # DQN
          q_target = self.qf_target(obs2)
       elif self.args.algo == 'ddqn':   # Double DQN
-         q2 = self.qf(obs2)
-         q_target = self.qf_target(obs2)
-         q_target = q_target.gather(1, q2.max(1)[1].unsqueeze(1))
-      q_backup = rews + self.gamma*(1-done)*q_target.max(1)[0]
+         with torch.no_grad():
+            argmax = torch.argmax(self.qf(obs2), dim = 2)
+            q_target = self.qf_target(obs2)
+            q_target = q_target.gather(2, argmax.unsqueeze(2)).squeeze(-1)
+            q_target = q_target.mean(1, keepdim = False)
+         #q2 = self.qf(obs2)
+         #q_target = self.qf_target(obs2)
+         #q_target = q_target.gather(1, q2.max(1)[1].unsqueeze(1))
+      #q_backup = rews + self.gamma*(1-done)*q_target.max(1)[0]
+      q_backup = rews + self.gamma*(1-done)*q_target
+      q_backup = torch.stack([q_backup for _ in range(self.act_dim)], dim = 1)
       q_backup.to(self.device)
 
       if 0: # Check shape of prediction and target
@@ -158,45 +199,129 @@ class Agent(object):
       # Save loss
       self.q_losses.append(qf_loss.item())
 
-   def run(self, max_step, state, action):
+   def run(self, data):
       step_number = 0
       total_reward = 0.
       done = False
 
+      obs = np.zeros(7)
+      obs[0] = data['encode_fps']
+      obs[1] = data['network_fps']
+      obs[2] = data['big_temp']/1000
+      obs[3] = data['modem_temp']/1000
+      obs[4] = data['big_clock2']/10000
+      obs[5] = data['res']/100
+      obs[6] = data['bitrate']
+
       # Keep interacting until agent reaches a terminal state.
       #while not (done or step_number == max_step):
+
+      obs = np.expand_dims(obs, axis = 0)
 
       if self.eval_mode:
          q_value = self.qf(torch.Tensor(obs).to(self.device))
          q_argmax = torch.argmax(q_value.squeeze(0), dim = 1)
          action = q_argmax.detach().cpu().numpy()    
-         next_obs, reward, done, _ = self.env.step(action)
+         #next_obs, reward, done, _ = self.env.step(action)
       else:
-         self.steps += 1
-
          # Collect experience (s, a, r, s') using some policy
-         action = self.select_action(torch.Tensor(obs).to(self.device))
-         next_obs, reward, done, _ = self.env.step(action)
-      
+         action = self.select_action(torch.Tensor(obs).to(self.device), data)
+
+         # This code is for test (220610)
+         '''
+         if self.steps % 6 == 0:
+            action = np.array([1,1])
+         else:
+            action = np.array([0,1])
+         '''
+
+         #next_obs, reward, done, _ = self.env.step(action)
+         reward = self.reward_calculator(data)
          # Add experience to replay buffer
-         self.replay_buffer.add(obs, action, reward, next_obs, done)
+         if self.steps >= 1:
+            print('Timestep:', self.steps*2, 'Prev obs: ', self.prev_obs, " Next obs: ", obs, " Action: ", self.prev_action - 1, 
+            " Reward: ", reward, " LossQ: ", round(np.mean(self.q_losses), 5))
+
+            self.replay_buffer.add(self.prev_obs, self.prev_action, reward, obs, done)
+
+            ## delayed penalty reward for high temperature
+            sample_to_save = {}
+            sample_to_save['prev_obs'] = self.prev_obs
+            sample_to_save['action'] = self.prev_action
+            sample_to_save['reward'] = reward - 2
+            sample_to_save['next_obs'] = obs
+            sample_to_save['done'] = done
+            self.save_current_samples(sample_to_save) 
+
+            if self.prev_obs[3] <51 and obs[3] >= 51:
+               print('Timestep:', self.steps*2, 'Delayed penealtiy reward for high temperature activated!')
+               for  data in self.current_buffer:
+                  if data != None:
+                     self.replay_buffer.add(data['prev_obs'], data['action'], data['reward'], data['next_obs'], data['done'])
          
          # Start training when the number of experience is greater than batch_size
-         if self.steps > self.batch_size:
+         if self.steps > self.batch_size + 1:
             self.train_model()
 
       total_reward += reward
       step_number += 1
-      obs = next_obs
       
+      self.steps += 1
+      self.prev_obs = obs
+      self.prev_action = action
+
       # Save logs
       self.logger['LossQ'] = round(np.mean(self.q_losses), 5)
       #return step_number, total_reward
 
-class agent_runner ():
-   self.agent = Agent ()
+      if self.steps % 100 == 0:
+         torch.save(self.qf.state_dict(), "./save_model/model" +self.save_name+".pt")
+         print("[Save model]")
+
+      return action, reward
+
+   def reward_calculator(self, data):
+      # fps reward
+      fps_reward = min(data['network_fps']/self.target_fps,1)
+      # resolution reward
+      res_reward = min(data['res']/self.max_res, 1)
+      # bitrate reward
+      br_reward = min(data['bitrate']/70, 1)
+      #res_reward = min(data['res'/self.target_bitrate], 1)
+      qoe_reward = fps_reward + self.beta1*res_reward + self.beta2*res_reward
+      
+      ## temperature reward
+      temp_reward = 0
+      if data['modem_temp']/1000 >= 51:
+         temp_reward = -2
+
+      return qoe_reward + temp_reward
+
+   def save_current_samples (self, data):
+      self.current_buffer[self.current_buffer_index] = data
+      self.current_buffer_index += 1
+      self.current_buffer_index %= self.max_current_buffer_size
 
 
-   def run_train ():
+class agent_runner:
+   def __init__(self):
+      args = easydict.EasyDict({
+         "algo": 'ddqn',
+         "load_saved": False,
+         "load_path": None,
+         "gpu_index": 0
+      })
+      device = torch.device('cuda', index=args.gpu_index) if torch.cuda.is_available() else torch.device('cpu')
+      #device = torch.device('cpu')
+      obs_dim = 7
+      act_dim = 2
+      act_num = 3
 
-   def run_inference ():
+      self.agent = Agent (args, device, obs_dim, act_dim ,act_num)
+      print('Start to run RL agent')
+
+   def run_train(self, data):
+      action, reward = self.agent.run(data)
+      client_action = action - 1
+      #print('Obs: ', data, 'Action: ', client_action, 'Reward: ', reward)
+      return client_action
--- http_server.py
+++ http_server.py
@@ -1,11 +1,14 @@
 from aiohttp import web
-#import multipart as mp
-#from multipart import tob
-import time
 from starlette.responses import PlainTextResponse, Response, JSONResponse
 from django.http import HttpResponse
+
+from bdq import *
+
+import time
 import json
 import os
+import threading
+
 os.environ.get("DJANGO_SETTINGS_MODULE")
 
 last_milli = 0
@@ -13,6 +16,7 @@ fps = 0
 counter = 0
 th = 0
 recv_size = 0
+rl_agent = agent_runner()
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -21,17 +25,25 @@ async def handle(request):
     return web.Response(text=text)
 
 async def rl_handler(request):
-    await request.post()
+    global fps
+    global th
 
-    print(request)
+    data = await request.json()
+    data['network_fps'] = fps
+    data['bitrate'] = th
+
+    actions = rl_agent.run_train(data)
 
     response = {}
-    response['res'] = 0
+    response['res'] = actions[0]
     response['bitrate'] = 0
-    response['little_clock'] = 0
-    response['big_clock'] = 0
+    response['clock'] = actions[1]
 
-    return web.json_response(response)
+    data = {"res": int(actions[0]), "bitrate": int(0), "clock":  int(actions[1])}
+
+    #print(data)
+
+    return web.json_response(data)
 
 
 async def post_handler(request):
@@ -43,29 +55,50 @@ async def post_handler(request):
 
     await request.post()
 
+    counter += 1
+    recv_size += int(request.headers.get("content-length"))
+    '''
     content_upload_time = current_milli_time()
     object_size = int(request.headers.get("content-length"))
 
-    if (content_upload_time - last_milli > 1000):
+    if (content_upload_time - last_milli > 2000):
         time_gap = content_upload_time - last_milli 
         last_milli = content_upload_time
         fps = round(counter/((time_gap)/1000),2)
-        th = round(recv_size*8/1e6,2)
+        th = round(recv_size*8/2e6,2)
         counter = 0
         recv_size = 0
     else:
         counter += 1
         recv_size += object_size
+    '''
 
     media_type = request.headers.get("content-type")
     content_upload_time = current_milli_time()
-    print('Object size: ', object_size/1e6, fps, th)
+    #print('Object size: ', object_size/1e6, fps, th)
 
     data = {"fps": int(fps), "th": round(float(th),2)}
 
     response = web.json_response(data)
 
     return response
+
+def calculate_fps():
+    global counter
+    global recv_size
+    global fps
+    global th
+
+    sampling_sec = 2
+
+    print('Start performance measurement thread')
+
+    while(1):
+        time.sleep(sampling_sec)
+        fps = round(counter/sampling_sec,2)
+        th = round(recv_size*8/2e6,2)
+        counter = 0
+        recv_size = 0
 
 def current_milli_time():
     return int(round(time.time()*1000))
@@ -76,4 +109,6 @@ app.add_routes([web.get('/', handle),
                 web.post('/video', post_handler)])
 
 if __name__ == '__main__':
-    web.run_app(app, port = 3030)
+    t1 = threading.Thread(target = calculate_fps)
+    t1.start()
+    web.run_app(app, port = 8888)
